a2b072fd3e9c2e6aa644ac1d4f6f07b460de5c96
rabbitty <rabbitty@burrow>
2025-05-08 02:22:40 -0600

    Best intent

--- fulanito/main.cpp
+++ fulanito/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
+#include <vector>
 
-int binarySearch(int i, int d, int valor, int *p);
+int binarySearch(int i, int d, int valor, const std::vector<int>& p);
 
 int main(){
-    int Q, N, acumulado=0, posicion =-1; 
-    std::cin >> Q >> N; 
-    int litros_persona[N];
+    int Q, N, acumulado=0, posicion =-1;
+    std::cin >> Q >> N;
+    // int litros_persona[N];
+    std::vector<int> litros_persona(N);
     for( int i=0; i<N; i++ ){
         int aux=0;
         std::cin >> aux;
@@ -17,32 +19,34 @@ int main(){
     // std::cout << std::endl;
     while(Q--){
         int aux = 0;
-        std::cin >> aux; 
+        std::cin >> aux;
         posicion = binarySearch(0, N-1, aux, litros_persona );
-        if( posicion == -1 ){
-            std::cout << (posicion+1) << " " << aux << std::endl;    
-        } else {
-            std::cout << (posicion+1) << " " << ( aux-litros_persona[posicion] ) << std::endl;
+        std::cout << posicion+1 << " ";
+        if(posicion == -1){
+            std::cout << aux << "\n";
+        }else {
+            std::cout << aux-litros_persona[posicion] << "\n";
         }
     }
-    
+
 
     return 0;
 }
 
-int binarySearch(int i, int d, int valor, int *p){
-
-    if( *(p+d)< valor ){
+int binarySearch(int i, int d, int valor, const std::vector<int>& p){
+    // std::cout << "\t-" << i << " " << d << " " << valor << std::endl;
+    if( p[d]< valor ){
         return d;
     }
-    while(i>d){
+
+    while( i <= d ){
         int medio = (i+d)/2;
-        if( *(p+medio) == valor )
+        if( p[medio] == valor )
             return medio;
-        else if( *(p+medio) < valor ){
+        else if( p[medio] < valor ){
             // Si en la siguiente iteración, el valor es mayor,
-            // no tiene caso seguir buscando. 
-            if( *(p+medio+1) > valor ){ 
+            // no tiene caso seguir buscando.
+            if( p[medio+1] > valor ){
                 return medio;
             } else {
                 i=medio+1;
