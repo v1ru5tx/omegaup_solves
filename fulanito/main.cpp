2b24aa26553c334994583ec62e9fcfa3bf372325
rabbitty <rabbitty@burrow>
2025-05-12 16:04:56 -0600

    Improve

--- fulanito/main.cpp
+++ fulanito/main.cpp
@@ -1,59 +1,45 @@
 #include <iostream>
 #include <vector>
 
-int binarySearch(int i, int d, int valor, const std::vector<int>& p);
+int binarySearch(int valor, const std::vector<int>& p) {
+    int i = 0, d = p.size() - 1;
+    if (valor > p[d]) return d; // Si el valor es mayor que el último acumulado.
 
-int main(){
-    int Q, N, acumulado=0, posicion =-1;
-    std::cin >> Q >> N;
-    // int litros_persona[N];
-    std::vector<int> litros_persona(N);
-    for( int i=0; i<N; i++ ){
-        int aux=0;
-        std::cin >> aux;
-        acumulado+=aux;
-        litros_persona[i] = acumulado;
+    while (i <= d) {
+        int medio = i + (d - i) / 2;
+        if (p[medio] == valor) return medio;
+        else if (p[medio] < valor) i = medio + 1;
+        else d = medio - 1;
     }
-    // for( int e:litros_persona )
-    //     std::cout << e << " | ";
-    // std::cout << std::endl;
-    while(Q--){
-        int aux = 0;
-        std::cin >> aux;
-        posicion = binarySearch(0, N-1, aux, litros_persona );
-        std::cout << posicion+1 << " ";
-        if(posicion == -1){
-            std::cout << aux << "\n";
-        }else {
-            std::cout << aux-litros_persona[posicion] << "\n";
-        }
-    }
-
-
-    return 0;
+    return d; // Devuelve el último índice menor al valor buscado.
 }
 
-int binarySearch(int i, int d, int valor, const std::vector<int>& p){
-    // std::cout << "\t-" << i << " " << d << " " << valor << std::endl;
-    if( p[d]< valor ){
-        return d;
+#include <iostream>
+#include <vector>
+#include <numeric>
+
+int main() {
+    int Q, N;
+    std::cin >> Q >> N;
+    std::vector<int> litros_persona(N);
+
+    for (int i = 0; i < N; ++i) {
+        int aux;
+        std::cin >> aux;
+        litros_persona[i] = aux;
+    }
+    std::partial_sum(litros_persona.begin(), litros_persona.end(), litros_persona.begin());
+
+    while (Q--) {
+        int aux;
+        std::cin >> aux;
+        int posicion = binarySearch(aux, litros_persona);
+        std::cout << posicion + 1 << " ";
+        if (posicion >= 0 && posicion < N)
+            std::cout << aux - litros_persona[posicion] << "\n";
+        else
+            std::cout << aux << "\n";
     }
 
-    while( i <= d ){
-        int medio = (i+d)/2;
-        if( p[medio] == valor )
-            return medio;
-        else if( p[medio] < valor ){
-            // Si en la siguiente iteración, el valor es mayor,
-            // no tiene caso seguir buscando.
-            if( p[medio+1] > valor ){
-                return medio;
-            } else {
-                i=medio+1;
-            }
-        }else
-            d=medio-1;
-
-    }
-    return -1;
+    return 0;
 }
